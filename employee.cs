eef195d5e01f56b1390673653c796b0a2ef39202
Levva01 <vellanifederico13@gmail.com>
2022-09-30 15:17:48 +0200

    Aggiungere i file di progetto.

---
+++ employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_ecommerce_db
+{
+    [Table("employee")]
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string name { get; set; }
+        public string surname { get; set; }
+        public string leve { get; set; }
+    }
+}
---
+++ orders.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_ecommerce_db
+{
+    [Table("orders")]
+    public class Orders
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public double Amount { get; set; }
+        public string Status { get; set; }
+    }
+}
