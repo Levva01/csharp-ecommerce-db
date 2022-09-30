91d083e21cef4665ae218b288cbbf08e3e4b0ec1
Levva01 <vellanifederico13@gmail.com>
2022-09-30 16:14:52 +0200

    context created

---
+++ ECommerceContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_ecommerce_db
+{
+    internal class ECommerceContext : DbContext
+    {
+        public DbSet<Customers> Customers { get; set; }
+        public DbSet<Orders> Orders { get; set; }
+        public DbSet<Products> Products { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Payment> Payments { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=localhost;Initial Catalog=db_ecommerce;Integrated Security=True");
+        }
+    }
+}
